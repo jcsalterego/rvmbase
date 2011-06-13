8769c3e1387c6d2224f425f3ca75b399b3887ba7
Jerry Chen <jerry@apache.org>
2011-06-13 16:30:51 +0000

    Internet-capable bootstrap.sh

---
+++ bootstrap.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+#
+# Run this via:
+#
+#   bash < <(curl -s https://raw.github.com/jcsalterego/rvmbase/master/bootstrap.sh)
+#
+# (Or y'know... don't.)
+#
+
+destdir=/usr/local/rvmbase
+tmpdir=/tmp/rvmbase.$$/
+
+[ -z "$(which git)" ] && echo "Requires git." && exit 1;
+[ -d "$destdir" ] && echo "$destdir already exists. Aborting." && exit 1;
+
+mkdir -p "$tmpdir"
+cd "$tmpdir";
+git clone https://github.com/jcsalterego/rvmbase.git;
+mkdir -p /opt
+cp -r $tmpdir/rvmbase $destdir
+cd $destdir;
+rm -rf $tmpdir;
+
+echo "Ready. Install with:"
+echo "  $ /usr/local/rvmbase/install.sh 1.9.2"
